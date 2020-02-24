e68eb7f38b669f9f4817a96adc99f6a8aa3e90b2
peterpeterparker <david.dalbusco@outlook.com>
2020-02-24 11:19:05 +0100

    feat: optional terminal style

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const toString = require("mdast-util-to-string")
 
 const _ = require(`lodash`)
 
-module.exports = ({ markdownAST }, _pluginOptions) => {
+module.exports = ({ markdownAST }, pluginOptions) => {
+
+    const terminal = pluginOptions && pluginOptions.terminal && pluginOptions.terminal !== '' ? pluginOptions.terminal : undefined
+
     visit(markdownAST, "code", node => {
         const text = toString(node)
 
         const html = `
-        <deckgo-highlight-code ${node && node.lang !== null ? `language="${node.lang}"` : ''}>
+        <deckgo-highlight-code ${node && node.lang !== null ? `language="${node.lang}"` : ''} ${terminal !== undefined ? `terminal="${terminal}"` : ''}>
           <code slot="code">${_.escape(text)}</code>
         </deckgo-highlight-code>
       `
