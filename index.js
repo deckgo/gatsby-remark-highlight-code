f5385636692907b7b293b405ffe6ea726c7507e1
peterpeterparker <david.dalbusco@outlook.com>
2020-04-23 08:01:50 +0200

    feat: format code

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-const visit = require("unist-util-visit")
-const toString = require("mdast-util-to-string")
+const visit = require("unist-util-visit");
+const toString = require("mdast-util-to-string");
 
-const _ = require(`lodash`)
+const _ = require(`lodash`);
 
 module.exports = ({ markdownAST }, pluginOptions) => {
-
-    visit(markdownAST, "code", node => {
-        const text = toString(node)
-        const properties = generatePropsString(pluginOptions) 
-        const html = `
-        <deckgo-highlight-code ${node && node.lang !== null ? `language="${node.lang}"` : ''}  ${properties}>
+  visit(markdownAST, "code", (node) => {
+    const text = toString(node);
+    const properties = generatePropsString(pluginOptions);
+    const html = `
+        <deckgo-highlight-code ${
+          node && node.lang !== null ? `language="${node.lang}"` : ""
+        }  ${properties}>
           <code slot="code">${_.escape(text)}</code>
         </deckgo-highlight-code>
-      `
+      `;
 
-        node.type = "html"
-        node.children = undefined
-        node.value = html
-    })
+    node.type = "html";
+    node.children = undefined;
+    node.value = html;
+  });
 
-    return markdownAST
-}
-
+  return markdownAST;
+};
 
 function generatePropsString(pluginOptions) {
   if (!pluginOptions) {
-    return ""
+    return "";
   }
-  let str = ""
+  let str = "";
   const { terminal, lineNumbers, editable } = pluginOptions;
 
   if (terminal) {
-    str += `terminal="${pluginOptions.terminal}" `
+    str += `terminal="${pluginOptions.terminal}" `;
   }
 
   if (lineNumbers === true) {
-    str += `line-numbers="true" `
+    str += `line-numbers="true" `;
   }
 
   if (editable === true) {
-    str += `editable="true" `
+    str += `editable="true" `;
   }
   return str;
 }
-
